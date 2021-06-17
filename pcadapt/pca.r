86db7a4eadbddf7d033def8b1e458dcbde4f8f00
Eric Morrison <emorrison40@gmail.com>
2021-06-17 10:10:46 -0400

    plink BED

--- pcadapt/pca.r
+++ pcadapt/pca.r
@@ -1,5 +1,10 @@
 require(pcadapt)
-path_to_file <- "Nf_post_SPANDx/LD_filter/test_dat.ped"
-filename <- read.pcadapt(path_to_file, type = "ped") #This looks like prefers conversion to BED format first
+path_to_file <- "Nf_post_SPANDx/LD_filter/test_dat.bed"
+filename <- read.pcadapt(path_to_file, type = "bed") #This looks like prefers conversion to BED format first
+
+#To check bed file
+
+bed_mat = pcadapt::bed2matrix(filename)
 
 x <- pcadapt(input = filename, K = 20, ploidy = 1)
+pca(filename, scale = T)
